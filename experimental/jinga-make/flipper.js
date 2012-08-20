be34bf3b2f21ff3395284040d47e8323c605183f
Sam Hart <457205+criswell@users.noreply.github.com>
2012-08-20 13:47:26 -0400

    flesh out the make test

---
+++ experimental/jinga-make/flipper.js
@@ -0,0 +1,10 @@
+function unFlip(n) {
+  var detail = document.getElementById(n);
+
+  if (detail.style.display == "inline") {
+      detail.style.display = "none";
+  } else {
+      detail.style.display = "inline";
+  }
+}
+
